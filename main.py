9b2a91cb6d17c358cdcaaeb27405edfc41edd8e5
Svetlanamsv <svetikmsv34@gmail.com>
2020-10-19 19:51:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Input three number", end="\n")
+a=int(input())
+b=int(input())
+c=int(input())
+d=(a+c)/2
+if d==b:
+    print("YES, it is arithmetic progression")
+else:
+    print("No, it isn't arithmetic progression")
